1f84cddfa2f4adfcaf70e33b8be5030ef9286991
Jakub Suchy <jakub@dynamiteheads.com>
2010-05-01 00:14:47 +0200

    Made basic modalframe window opening working

--- brightcove_cck/js/browse.js
+++ brightcove_cck/js/browse.js
@@ -2,18 +2,11 @@ Drupal.brightcove_cck = {};
 Drupal.brightcove_cck.actions = {};
 
 Drupal.behaviors.brightcove_cck_buttons = function(context) {
-    $('.brightcove-cck-browse-button', context).click(Drupal.brightcove_cck.actions.browse);
+  $('.brightcove-cck-browse-button', context).click(Drupal.brightcove_cck.actions.browse);
 };
 
 
 Drupal.brightcove_cck.actions.browse = function() {
- var options = {
-      // TODO these options need to be (1) specific to modalframe dialog instances
-      // and (2) presented in the settings form or translated from the options
-      // there. autoFit is necessary to prevent the dialog from changing size all
-      // the time.
-url: 'brightcove_cck/browse',
-autoFit: false
-};
-Drupal.modalFrame.open({url: 'brightcove_cck/browse'});
+  Drupal.modalFrame.open({url: Drupal.settings.basePath + 'brightcove_cck/browse', width: 800, height: 600, autoFit: false});
+  return false;
 }
