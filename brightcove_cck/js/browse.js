5ddb8242ed367ca0c83047e0d8b96c9e22dce61e
Jakub Suchy <jakub@dynamiteheads.com>
2010-05-06 18:52:37 +0200

    Added all possible formatters - stubs

--- brightcove_cck/js/browse.js
+++ brightcove_cck/js/browse.js
@@ -1,10 +1,24 @@
+/**
+  * @file browse.js
+  * Creates actions - browse and remove for Video form fields.
+  */
+
 Drupal.brightcove_cck = {};
 Drupal.brightcove_cck.actions = {};
 
 Drupal.behaviors.brightcove_cck_buttons = function(context) {
   $('.brightcove-cck-browse-button', context).click(Drupal.brightcove_cck.actions.browse);
+  $('.brightcove-cck-remove-button', context).click(Drupal.brightcove_cck.actions.remove);
+  // TODO: There should be a better selector for this - how do we add a class to cck element? (attributes didn't work)
+  $('.form-text.form-autocomplete').change(Drupal.brightcove_cck.actions.change);
 };
 
+Drupal.brightcove_cck.actions.change = function() {
+  $('.brightcove-cck-remove-button[rel="' + $(this).attr('id') + '"]').attr('disabled', '');
+}
+Drupal.brightcove_cck.actions.remove = function() {
+  $('#' + $(this).attr('rel')).val('');
+}
 
 Drupal.brightcove_cck.actions.browse = function() {
   var id = $(this).attr('rel');
@@ -14,6 +28,7 @@ Drupal.brightcove_cck.actions.browse = function() {
 
 Drupal.brightcove_cck.submit = function(settings) {
   return function(args) {
-          $("#" + settings).val(args.selected)
+    $("#" + settings).val(args.selected)
+    $('.brightcove-cck-remove-button[rel="' + settings + '"]').attr('disabled', '');
   };
 }
