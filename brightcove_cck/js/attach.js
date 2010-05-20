2f80060fa764b5e6ca945f9948bcdf0997a5446e
Jakub Suchy <jakub@dynamiteheads.com>
2010-05-20 14:55:04 +0200

    Implemented a video browser pager

--- brightcove_cck/js/attach.js
+++ brightcove_cck/js/attach.js
@@ -11,3 +11,14 @@ Drupal.brightcove_cck.submit = function(settings) {
           alert(args);
   };
 }
+/**
+  * Overriding Drupal.modalFrameChild.behaviors.parseLinks function from
+  * modalframe in order to ensure that pager links do not point to
+  * target="_new"
+  * Just return for now.
+  */
+Drupal.modalFrameChild.behaviors.parseLinks = function(context) {
+  return;
+};
+
+
