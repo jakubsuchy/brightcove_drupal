0f701cc30a4ae18f0ac162b8dfa5a0f004f9c9d7
Jakub Suchy <jakub@dynamiteheads.com>
2010-05-24 17:32:57 +0200

    Added a "image" formatter without a link to node.

---
+++ brightcove_cck/brightcove-cck-node-image.tpl.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+
+/**
+ * @file brightcove-cck-node-image.tpl.php
+ * Image linked to node theme implementation.
+ *
+ * This template is used when viewing a Brightcove video with a thumbnail or videoStill image linking to node.
+ *
+ * Available variables:
+ *   - $output: Formatted image with a link to Lightbox2, ready for printing.
+ *   - $image_field: The name of the image field used from Media API. Can be thumbnailURL or videoStillURL.
+ *   - $nid: Node NID.
+ *   - $video_id: Video ID from Media API.
+ *   - $field_name: CCK Field name.
+ *   - $type_name: Node type.
+ *
+ * @see theme_brightcove_cck_node_image()
+ */
+?>
+
+<?php if ($output): ?>
+<?php echo $output; ?>
+<?php endif; ?>
